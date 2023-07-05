6ce44ab6b6dc7da69dc42c660a11b3ee47e50215
Joel Berendzen <joel@generisbio.com>
2023-07-05 10:54:41 -0600

    further file refactoring

--- src/flardl/multidispatcher.py
+++ src/flardl/multidispatcher.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import sys
-from typing import Any
 
 # third-party imports
 import anyio
@@ -139,87 +138,3 @@ class MultiDispatcher:
         return anyio.run(
             self.run, arg_list, backend=backend, backend_options=backend_options
         )
-
-
-class StreamWorker:
-    """Basic worker functions."""
-
-    def __init__(
-        self, name: str, logger: loguru.Logger | None = None, quiet: bool = False
-    ):
-        """Init data structures."""
-        if logger is None:
-            self._logger = mylogger
-        else:
-            self._logger = logger
-        self.name = name
-        self.n_soft_fails = 0
-        self.n_hard_fails = 0
-        self.hard_exceptions: tuple[()] | tuple[type[BaseException]] = ()
-        self.soft_exceptions: tuple[()] | tuple[type[BaseException]] = ()
-        self.work_qty_name = "bytes"
-        self.quiet = quiet
-
-    async def queue_results(
-        self,
-        result_q: ResultStream,
-        worker_count: int,
-        idx: int,
-        work_qty: float | int,
-        /,
-        **kwargs: SIMPLE_TYPES,
-    ):
-        """Put dictionary of results on ouput queue."""
-        results = {
-            INDEX_KEY: idx,
-            "worker": self.name,
-            self.work_qty_name: work_qty,
-        }
-        results.update(kwargs)
-        await result_q.put(results, worker_name=self.name, worker_count=worker_count)
-
-    async def hard_exception_handler(
-        self,
-        index: int,
-        worker_name: str,
-        worker_count: int,
-        error: Exception,
-        failure_q: FailureStream,
-    ):
-        """Handle exceptions that re-queue arguments as failed."""
-        if error.__class__ in self.soft_exceptions:
-            message = repr(error)
-            error_name = "TooManyRetries"
-        else:
-            message = str(error)
-            error_name = error.__class__.__name__
-        if not self.quiet:
-            self._logger.error(f"{error_name}: {message}")
-        failure_entry = {
-            INDEX_KEY: index,
-            "worker": worker_name,
-            "error": error_name,
-            "message": message,
-        }
-        await failure_q.put(
-            failure_entry, worker_name=worker_name, worker_count=worker_count
-        )
-
-    async def unhandled_exception_handler(self, index: int, error: Exception):
-        """Handle unhandled exceptions."""
-        self._logger.error(error)
-        await self._logger.complete()
-        sys.exit(1)
-
-    async def soft_exception_handler(
-        self,
-        kwargs: dict[str, Any],
-        worker_name: str,
-        worker_count: int,
-        error: Exception,
-        arg_q: ArgumentStream,
-    ):
-        """Handle exceptions that re-try arguments."""
-        if not self.quiet:
-            self._logger.warning(error)
-        await arg_q.put(kwargs, worker_name=worker_name, worker_count=worker_count)
