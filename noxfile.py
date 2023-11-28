5b6d657cea56ef7bbfab5869bb634864dfea1cf6
Joel Berendzen <joel@generisbio.com>
2023-11-28 15:32:05 -0700

    pdm in nox

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,14 @@
 """Nox sessions."""
 import os
 import random
-import shlex
 import shutil
 import sys
-from collections.abc import Iterable
-from collections.abc import Iterator
 from pathlib import Path
-from textwrap import dedent
 
 import nox
 
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 package = "flardl"
 python_versions = ["3.12", "3.11", "3.10", "3.9"]
@@ -26,146 +24,6 @@ nox.options.sessions = (
 )
 
 
-def install(session: nox.Session, *, groups: Iterable[str], root: bool = True) -> None:
-    """Install the dependency groups using Poetry.
-
-    This function installs the given dependency groups into the session's
-    virtual environment. When ``root`` is true (the default), the function
-    also installs the root package and its default dependencies.
-    To avoid an editable install, the root package is not installed using
-    ``poetry install``. Instead, the function invokes ``pip install .``
-    to perform a PEP 517 build.
-
-    Args:
-        session: The Session object.
-        groups: The dependency groups to install.
-        root: Install the root package.
-    """
-    session.run_always(
-        "poetry",
-        "install",
-        "--no-root",
-        "--sync",
-        "--{}={}".format("only" if not root else "with", ",".join(groups)),
-        external=True,
-    )
-    if root:
-        session.install(".")
-
-
-def export_requirements(session: nox.Session, *, extras: Iterable[str] = ()) -> Path:
-    """Export a requirements file from Poetry.
-
-    This function uses ``poetry export`` to generate a requirements file
-    containing the default dependencies at the versions specified in
-    ``poetry.lock``.
-
-    Args:
-        session: The Session object.
-        extras: Extras supported by the project.
-
-    Returns:
-        The path to the requirements file.
-    """
-    output = session.run_always(
-        "poetry",
-        "export",
-        "--format=requirements.txt",
-        "--without-hashes",
-        *[f"--extras={extra}" for extra in extras],
-        external=True,
-        silent=True,
-        stderr=None,
-    )
-
-    if output is None:
-        session.skip(
-            "The command `poetry export` was not executed"
-            " (a possible cause is specifying `--no-install`)"
-        )
-
-    assert isinstance(output, str)  # noqa: S101
-
-    def _stripwarnings(lines: Iterable[str]) -> Iterator[str]:
-        for line in lines:
-            if line.startswith("Warning:"):
-                print(line, file=sys.stderr)
-                continue
-            yield line
-
-    text = "".join(_stripwarnings(output.splitlines(keepends=True)))
-
-    path = session.cache_dir / "requirements.txt"
-    path.write_text(text)
-
-    return path
-
-
-def activate_virtualenv_in_precommit_hooks(session: nox.Session) -> None:
-    """Activate virtualenv in hooks installed by pre-commit.
-
-    This function patches git hooks installed by pre-commit to activate the
-    session's virtual environment. This allows pre-commit to locate hooks in
-    that environment when invoked from git.
-
-    Args:
-        session: The Session object.
-    """
-    assert session.bin is not None  # noqa: S101
-
-    # Only patch hooks containing a reference to this session's bindir. Support
-    # quoting rules for Python and bash, but strip the outermost quotes so we
-    # can detect paths within the bindir, like <bindir>/python.
-    bindirs = [
-        bindir[1:-1] if bindir[0] in "'\"" else bindir
-        for bindir in (repr(session.bin), shlex.quote(session.bin))
-    ]
-
-    virtualenv = session.env.get("VIRTUAL_ENV")
-    if virtualenv is None:
-        return
-
-    headers = {
-        # pre-commit >= 2.16.0
-        "bash": f"""\
-            VIRTUAL_ENV={shlex.quote(virtualenv)}
-            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
-            """,
-        # pre-commit >= 2.17.0 on Windows forces sh shebang
-        "/bin/sh": f"""\
-            VIRTUAL_ENV={shlex.quote(virtualenv)}
-            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
-            """,
-    }
-
-    hookdir = Path(".git") / "hooks"
-    if not hookdir.is_dir():
-        return
-
-    for hook in hookdir.iterdir():
-        if hook.name.endswith(".sample") or not hook.is_file():
-            continue
-
-        if not hook.read_bytes().startswith(b"#!"):
-            continue
-
-        text = hook.read_text()
-
-        if not any(
-            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
-            for bindir in bindirs
-        ):
-            continue
-
-        lines = text.splitlines()
-
-        for executable, header in headers.items():
-            if executable in lines[0].lower():
-                lines.insert(1, dedent(header))
-                hook.write_text("\n".join(lines))
-                break
-
-
 @nox.session(name="pre-commit", python=python_versions[0])
 def precommit(session: nox.Session) -> None:
     """Lint using pre-commit."""
@@ -175,26 +33,25 @@ def precommit(session: nox.Session) -> None:
         "--hook-stage=manual",
         "--show-diff-on-failure",
     ]
-    install(session, groups=["pre-commit"], root=False)
+    session.run_always("pdm", "install", "-G", "pre-commit", external=True)
     session.run("pre-commit", *args)
-    if args and args[0] == "install":
-        activate_virtualenv_in_precommit_hooks(session)
 
 
 @nox.session(python=python_versions[0])
 def safety(session: nox.Session) -> None:
     """Scan dependencies for insecure packages."""
-    # NOTE: Pass `extras` to `export_requirements` if the project supports any.
-    requirements = export_requirements(session)
-    install(session, groups=["safety"], root=False)
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run_always("pdm", "install", "-G", "safety", external=True)
+    session.run_always("pdm", "export", "-o", "requirements.txt", "--without-hashes")
+    session.run("safety", "check", "--full-report")
+    Path("requirements.txt").unlink()
 
 
 @nox.session(python=python_versions)
 def mypy(session: nox.Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src"]
-    install(session, groups=["mypy", "tests"])
+    session.run_always("pdm", "install", "-G", "tests",
+                       "-G", "mypy", external=True)
     session.run("mypy", *args)
     if not session.posargs:
         session.run(
@@ -208,7 +65,8 @@ def mypy(session: nox.Session) -> None:
 @nox.session(python=python_versions)
 def tests(session: nox.Session) -> None:
     """Run the test suite."""
-    install(session, groups=["coverage", "tests"])
+    session.run_always("pdm", "install", "-G", "tests",
+                       "-G", "coverage", external=True)
     try:
         session.run("coverage", "run", "-m", "pytest", *session.posargs)
         cov_path = Path(".coverage")
@@ -223,18 +81,19 @@ def tests(session: nox.Session) -> None:
 def coverage(session: nox.Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report"]
-    install(session, groups=["coverage"], root=False)
+    session.run_always("pdm", "install",
+                       "-G", "coverage", external=True)
 
     if not session.posargs and any(Path().glob(".coverage.*")):
         session.run("coverage", "combine")
-
     session.run("coverage", *args)
 
 
 @nox.session(python=python_versions)
 def typeguard(session: nox.Session) -> None:
     """Runtime type checking using Typeguard."""
-    install(session, groups=["typeguard", "tests"])
+    session.run_always("pdm", "install", "-G", "tests",
+                       "-G", "typeguard", external=True)
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
@@ -247,8 +106,8 @@ def xdoctest(session: nox.Session) -> None:
         args = [f"--modname={package}", "--command=all"]
         if "FORCE_COLOR" in os.environ:
             args.append("--colored=1")
-
-    install(session, groups=["xdoctest"])
+    session.run_always("pdm", "install",
+                       "-G", "xdoctest", external=True)
     session.run("python", "-m", "xdoctest", *args)
 
 
@@ -258,12 +117,11 @@ def docs_build(session: nox.Session) -> None:
     args = session.posargs or ["docs", "docs/_build"]
     if not session.posargs and "FORCE_COLOR" in os.environ:
         args.insert(0, "--color")
-
-    install(session, groups=["docs"])
+    session.run_always("pdm", "install",
+                       "-G", "docs", external=True)
     build_dir = Path("docs", "_build")
     if build_dir.exists():
         shutil.rmtree(build_dir)
-
     session.run("sphinx-build", *args)
 
 
@@ -271,10 +129,9 @@ def docs_build(session: nox.Session) -> None:
 def docs(session: nox.Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
-    install(session, groups=["docs"])
-
+    session.run_always("pdm", "install",
+                       "-G", "docs", external=True)
     build_dir = Path("docs", "_build")
     if build_dir.exists():
         shutil.rmtree(build_dir)
-
     session.run("sphinx-autobuild", *args)
